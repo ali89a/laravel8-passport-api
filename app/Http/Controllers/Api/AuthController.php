cd082bc98ed5b73b8101ffd7ef92e17149b55f67
Mohammad Ali <mdali89a@gmail.com>
2021-07-13 22:51:43 +0600

    First commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        //       return $request->all();
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ]);
+        if ($validator->fails()) {
+            return send_error('Validation Error', $validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        DB::beginTransaction();
+
+        try {
+
+            $request['password'] = Hash::make($request['password']);
+            $user = User::create([
+                'name' => $request['name'],
+                'email' => $request['email'],
+                'password' => $request['password'],
+            ]);
+            $accessToken = $user->createToken('authToken')->accessToken;
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['success' => false, 'errors' => $e->getMessage()], 500);
+        }
+
+        $data = [
+            'access_token' => $accessToken,
+            'userData' => $user,
+        ];
+        return send_response('Registration success.', $data, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
+            // return send_error('Validation Error',$validator->errors(), 422);
+            return response()->json(['success' => false, 'errors' => $validator->errors(),], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::where('email', $request->email)->first();
+            $accessToken = $user->createToken('authToken')->accessToken;
+
+            $data = [
+                'access_token' => $accessToken,
+                'userData' => $user,
+            ];
+            return send_response('Login successfully.', $data, Response::HTTP_CREATED);
+        }
+
+        return response()->json(['success' => false, 'errors' => 'email or password incorrect',], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout successfully'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $data = Category::all();
+        if ($data) {
+            return send_response('Successfully Retrive.', CategoryResource::collection($data), Response::HTTP_FOUND);
+        } else {
+            return send_error('Data Not Found!', [], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param   $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+        if ($category) {
+            return send_response('Successfully Retrive.', new CategoryResource($category), Response::HTTP_FOUND);
+        } else {
+            return send_error('Data Not Found!', [], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return send_response('Successfully Deleted.', new CategoryResource($category), Response::HTTP_FOUND);
+    }
+}
